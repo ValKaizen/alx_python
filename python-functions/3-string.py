49238d89689ff74dce67ad68eec8c694357adcd9
ValKaizen <allurevalora@gmail.com>
2023-07-20 03:18:53 -0700

    Python function called reverse_string that takes a string as input and returns the reverse of that string

---
+++ python-functions/3-string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+input_string = "Hello, World!"
+reversed_string = reverse_string(input_string)
+print(f"Original string: {input_string}")
+print(f"Reversed string: {reversed_string}")
