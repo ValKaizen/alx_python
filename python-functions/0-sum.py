b8c7f81bc61c1ab02b9de97c0ea6311dccfb1833
ValKaizen <allurevalora@gmail.com>
2023-07-20 00:21:02 -0700

    Function that adds two integers and returns the result

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,10 +1,9 @@
-
 #!/usr/bin/python3
-def add(a: any
-         b: any):
+def add(a, b):
     # Perform addition using bitwise operations
     while b != 0:
         carry = a & b
         a = a ^ b
         b = carry << 1
-return a
+
+    return a
