4fb8a3c506c5836d5f32911ea3cac374767c82ee
ValKaizen <allurevalora@gmail.com>
2023-07-20 00:38:21 -0700

    function that computes a to the power of b and return the value

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,3 +1,17 @@
-#!/usr/bin/python3
 def pow(a, b):
-    return (a ** b)
+    result = 1
+
+    # If the exponent is negative, invert base and exponent
+    if b < 0:
+        a = 1 / a
+        b = -b
+
+    while b > 0:
+        # If the exponent is odd, multiply result with base
+        if b % 2 == 1:
+            result *= a
+
+        a *= a
+        b //= 2
+
+    return result
