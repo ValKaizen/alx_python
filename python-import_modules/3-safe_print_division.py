f18756332db933a8b3e12671a70c928a9eee191a
ValKaizen <allurevalora@gmail.com>
2023-07-28 12:13:36 -0700

    Division Operation

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -2,7 +2,7 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        print("Error: Cannot divide by zero.")
+        #print("Error: Cannot divide by zero.")
         return None
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -10,8 +10,7 @@ def safe_print_division(a, b):
     else:
         print("Inside result: {}".format(result))
         return result
-    finally:
-        print("Finally: Division operation completed.")
+    #finally:print("Finally: Division operation completed.")
 
 # Test cases
 print(safe_print_division(10, 2))
