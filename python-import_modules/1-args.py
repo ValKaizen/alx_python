2139d0d4e04c9684f0c7796636af08a63fa7334b
ValKaizen <allurevalora@gmail.com>
2023-07-27 19:57:10 -0700

    arguments

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -8,7 +8,7 @@ def print_arguments():
 def print_arguments():
     num_args = len(sys.argv) - 1
     # Print the number of arguments and the appropriate plural/singular form
-    print("Number of argument{}: {}".format("s" if num_args != 1 else "", num_args))
+    print("argument{}: {}".format("s" if num_args != 1 else "", num_args))
 
     if num_args == 0:
         # If no arguments were passed, print a dot and a new line
@@ -17,9 +17,5 @@ def print_arguments():
         # For each argument, print its position, value, and a new line
         for i, arg in enumerate(sys.argv[1:], 1):
             print("{}: {}".format(i, arg))
-
-    # Restore the original sys.argv
-    sys.argv = original_args
-
 if __name__ == "__main__":
     print_arguments()
