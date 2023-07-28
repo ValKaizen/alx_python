3b5418f0a67f169f6ebd53a9d7e132ea937d7f75
ValKaizen <allurevalora@gmail.com>
2023-07-27 18:20:43 -0700

    import simple variable

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,4 +1,4 @@
 from variable_load_2 import a
 print(a)
 # Additional code outside the if __name__ == "__main__": block
-print("This code will be executed regardless of import or direct execution.")
+#print("This code will be executed regardless of import or direct execution.")
