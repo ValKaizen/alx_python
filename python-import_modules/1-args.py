d42ea025446a921756d2c9657ae6e756bc38f686
ValKaizen <allurevalora@gmail.com>
2023-07-27 19:18:18 -0700

    Argument

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,17 +1,18 @@
 import sys
 
-def main():
-    # Get the number of command-line arguments
+def print_arguments():
     num_args = len(sys.argv) - 1
 
-    # Print the number of arguments and the list of arguments
-    print("Number of argument{}: {}".format("s" if num_args != 1 else "", num_args))
+    # Print the number of arguments and the appropriate plural/singular form
+    print("Number of argument{}:{}".format("s" if num_args != 1 else "", num_args))
 
     if num_args == 0:
         print(".")
     else:
+        # For each argument, print its position, value, and a new line
         for i, arg in enumerate(sys.argv[1:], 1):
-            print("{}: {}".format(i, arg))
+            print("{}:{}".format(i, arg))
 
 if __name__ == "__main__":
-    main()
+    print_arguments()
+
