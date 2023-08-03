8dc2685b04b55695d98f6c904be49988f62281ea
ValKaizen <allurevalora@gmail.com>
2023-08-02 19:47:12 -0700

    import module

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -7,11 +7,11 @@ def safe_print_division(a, b):
     except TypeError:
         print("Error: Both inputs must be integers.")
         return None
-    #else:
-        #print("Inside result: {}".format(result))
+    else:
+        print("Inside result: {}".format(result))
         return result
     finally:
-        if isinstance(a, int) and isinstance(b, int):
-            print("{} / {} = {}".format(a, b, result))
+        #if isinstance(a, int) and isinstance(b, int):
+         #   print("{} / {} = {}".format(a, b, result))
         #else:
             #print("Invalid inputs: {} / {} cannot be calculated.".format(a, b))
