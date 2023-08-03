7477578c7aea57d4710bb2f4ca51a5f7aea7fd74
ValKaizen <allurevalora@gmail.com>
2023-08-02 20:02:33 -0700

    import module

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,17 +1,9 @@
+#!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
+        quotient = a / b
     except ZeroDivisionError:
-        print("Error: Division by zero is not allowed.")
-        return None
-    except TypeError:
-        print("Error: Both inputs must be integers.")
-        return None
-    else:
-        print("Inside result: {}".format(result))
-        return result
+        quotient = None
     finally:
-        if isinstance(a, int) and isinstance(b, int):
-          print("{} / {} = {}".format(a, b, result))
-        #else:
-            #print("Invalid inputs: {} / {} cannot be calculated.".format(a, b))
+        print("Inside result: {}".format(quotient))
+        return quotient
