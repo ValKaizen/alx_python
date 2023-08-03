55b24ebd3222c2a31b28dae2f1cbb48a487ba2b3
ValKaizen <allurevalora@gmail.com>
2023-08-02 20:05:19 -0700

    Import Modules

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,7 +1,3 @@
+#!/usr/bin/python3
 def raise_exception():
-    #raise TypeError("Exception has been raised")
-# Example usage: 
-try:
-    raise_exception()
-except TypeError as e:
-    print("Caught exception:", e)
+    raise TypeError
--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,22 +1,3 @@
+#!/usr/bin/python3
 def raise_exception_msg(message=""):
-    if len(message) < 9:
-        raise NameError(message[:9])
-    elif len(message) < 15:
-        raise NameError(message[:15])
-    else:
-        raise NameError(message[0])
-    # Test cases
-try:
-    raise_exception_msg("ShortMsg")
-except NameError as e:
-    print(str(e))
-
-try:
-    raise_exception_msg("MediumMessage")
-except NameError as e:
-    print(str(e))
-
-try:
-    raise_exception_msg("TooLongMessage")
-except NameError as e:
-    print(str(e))
+    raise NameError(message)
