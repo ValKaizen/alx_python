003e8617d4402b4d4f4d1df125b38c706600dd97
ValKaizen <allurevalora@gmail.com>
2023-08-14 03:52:42 -0700

    Class that defines the size of square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """This is the "square" module.
-This module provides a simple Square class with intialize sizee.
+This module provides a simple Square class with intialize size.
 Size defaults to 0. Raise errors on invalid inputs.
 """
 class Square:
     """A  class that defines a square by size"""
-    def __init__(self, size=0):
-        if type(size) != int:
+    def __init__(self, Square__size=0):
+        if type(Square__size) != int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if Square__size < 0:
             raise ValueError("size must be >= 0")
-        self.__size__ = size
+        self.__size__ = Square__size
+
+      
